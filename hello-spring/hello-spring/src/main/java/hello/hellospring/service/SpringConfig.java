2a1b46c5b57dc4d8613a8deda2e7a130c9bf8318
coldcow <93986255+coldcow@users.noreply.github.com>
2022-11-13 21:34:49 +0900

    commit

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,21 +1,18 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+//import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-
+    private final MemberRepository memberRepository; public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new MemberService(memberRepository);
     }
 }
